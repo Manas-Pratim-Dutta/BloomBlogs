e00a0866414bb43ee1c9e62ac5eaa493ee55a1fd
Manas Pratim Dutta <manaspratimdutta1999@gmail.com>
2024-01-13 15:51:31 +0530

    updated database configurations

---
+++ src/appwrite/config.js
@@ -0,0 +1,138 @@
+import conf from "../conf/conf.js"
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+
+
+export class Service {
+    client = new Client();
+    databases;
+    storage;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId)
+        this.databases = new Databases(this.client);
+        this.storage = new Storage(client);
+    }
+
+    async createPost({ title, slug, content, featuredImage, status, userId }) {
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug, // or we can use ID.Unique()
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite service :: createPost :: error", error);
+        }
+    }
+    // slug in place of documnet ID as per appwrite to uniquely identify what post to be updated.
+    async updatePost(slug, { title, content, featuredImage, status, userId }) {
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug, // the document ID 
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite service :: UpdatePost :: error", error);
+        }
+    }
+
+    async deletePost(slug) {
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite service :: createPost :: error", error);
+            return false
+        }
+    }
+
+    // get one document then we use-->
+
+    async getPost(slug) {
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+            )
+        } catch (error) {
+            console.log("Appwrite service :: getPost :: error", error);
+        }
+    }
+
+    // To get all post but with active status so we use query
+
+    async getPosts(queries = [Query.equal("status", "active")]) {
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries,
+            )
+        } catch (error) {
+            console.log("Appwrite service :: getPosts :: error", error);
+            return false
+        }
+    }
+
+    // file upload service
+
+    async uploadFiles(file) {
+        try {
+            return await this.storage.createFile(
+                conf.appwriteBucketId,
+                ID.unique(), //'[FILE_ID]' as per appwrite
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite service :: uploadFiles :: error", error);
+        }
+    }
+
+    // delete file service
+
+    async deleteFile(fileId) {
+        try {
+            await this.storage.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite service :: deleteFile :: error", error);
+        }
+    }
+
+    // file Preview
+
+    getFilePreview(fileId){
+        return this.storage.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
+    }
+}
+
+const service = new Service()
+export default service
