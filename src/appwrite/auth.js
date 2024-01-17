618a424e9af2bd0e3b87069c3fb4831e2d3a8e7a
Manas Pratim Dutta <manaspratimdutta1999@gmail.com>
2024-01-17 14:31:06 +0530

    minor change on 51

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -48,7 +48,7 @@ export class AuthService {
         try {
             await this.account.deleteSessions()
         } catch (error) {
-            throw error
+            console.log("Appwrite service :: logout:: error",error);
         }
     }
 }
